170402913d1fa1f16bd7be0ad07c9532d298a618
vivi31322 <59107738+vivi31322@users.noreply.github.com>
2020-04-15 23:09:23 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -59,12 +59,12 @@ def ml_loop():
             tx=ball_x-ball_past_x
             m=ty/tx
             platform_should_be=100
-            if ty>=0 or ball_y>250: 
+            if ty>=0 or ball_y>300: 
                 platform_should_be=((400-ball_y)/m)+ball_x
                 if (m*(200-ball_x)+ball_y<=400 and tx>0) : 
-                    platform_should_be=400-platform_should_be
+                    platform_should_be=400-platform_should_be/2
                 elif (m*(-ball_x)+ball_y<=400 and tx<0) :
-                    platform_should_be*=-1
+                    platform_should_be*=-1/2
             if platform_should_be<platform_x :
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
             elif platform_should_be>platform_x:
