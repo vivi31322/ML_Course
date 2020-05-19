624fbb088c2dcdb02a458243e6c6d9e88f0a06a9
vivi31322 <59107738+vivi31322@users.noreply.github.com>
2020-05-19 23:22:47 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -25,8 +25,8 @@ def ml_loop(side: str):
     block_pre=0
     def move_to(player, pred) : #move platform to predicted position to catch ball 
         if player == '1P':
-            if scene_info["platform_1P"][0]+20  > (pred-5) and scene_info["platform_1P"][0]+20 < (pred+5): return (random.random()*10%2)+1 # NONE
-            elif scene_info["platform_1P"][0]+20  > (pred-10) and scene_info["platform_1P"][0]+20 < (pred+10): return 0 # NONE
+            if scene_info["platform_1P"][0]+20  > (pred-5) and scene_info["platform_1P"][0]+20 < (pred+5): return 0 # NONE
+            elif scene_info["platform_1P"][0]+20  > (pred-10) and scene_info["platform_1P"][0]+20 < (pred+10): return (random.random()*10%2)+1 # NONE
             elif scene_info["platform_1P"][0]+20 <= (pred-10) : return 1 # goes right
             else : return 2 # goes left
         else :
